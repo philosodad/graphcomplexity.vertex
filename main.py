5e750a3a549ec66dba577608520cc43b05433030
j.paul daigle <jdaigle1@student.gsu.edu>
2009-12-09 02:47:36 -0500

    simplified structure, changed several dictionaries to arrays of objects

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import obal
 def main():
     sim.initialize()
     net = netw.NodeSource()
-    net.generate(50, 5)
+    net.generate(50, 10)
     sim.simulate(until=200)
 
 main()
--- obal.py
+++ obal.py
@@ -1,6 +1,6 @@
 import random as ran
 
 class G(object):
-    bound = 1000
+    bound = 800
     sensor_range = 80
     comm_range = 160
