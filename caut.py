82110eb8fafcb719879a2a4f66d03597384455cf
j.paul daigle <jdaigle1@student.gsu.edu>
2009-12-18 01:19:52 -0500

    The simulation works but the covers are terrible

--- caut.py
+++ caut.py
@@ -17,33 +17,21 @@ def t_algorithm(n):
         keyed_targets[x] = a
     if ran.randint(0,1) == 0:
         n.root = True
-        print ("root %d" %(n.id))
     else:
-        print "not root ", n.id
         n.root = False
-    print n.root
     if not n.root:
         for a in n.neighbors:
-            print a.root
             if a.root:
-                print a.id, " is a root!"
                 if step_check(n, a):
-                    print "step check returns true"
                     keyed_targets[a.id].active = True
             x = filter(lambda a: a.active, n.targets)
             if x:
                 x = ran.choice(x)
                 x.star_edge = True
-                print "a random choice has been made ", x.id, x.star_edge
-            else:
-                print "no active edges for ", n.id
     else:
-        print "process ", n.id
         if ran.randint(0,1) == 0:
             if not n.on:
-                print n.id, " is off"
                 for a in filter(lambda t: t.star_edge, n.targets):
-                    print "lets step"
                     step(keyed_neighbors[(a.uv - set([n.id])).pop()], n)
         else:
             if filter(lambda t: t.star_edge, n.targets):
@@ -52,10 +40,7 @@ def t_algorithm(n):
                     
 
 def step_check(v, w):
-    print v.ex
-    print "before calculation, beta should be ", (1-v.ex) * big-v.battery_life
     beta = min([(1-v.ex)*(big-v.battery_life), (1-w.ex)*(big-w.battery_life)])
-    print ("beta %s: %s" %(v.id, beta))
     if v.ex + (beta / float(big-v.battery_life)) == 1:
         return True
     else:
@@ -64,7 +49,6 @@ def step_check(v, w):
 
 def step(v,w):
     beta = min([(1-v.ex)*(big-v.battery_life), (1-w.ex)*(big-w.battery_life)])
-    print ("step beta %s: %s" %(v.id, beta))
     v.ex = v.ex + (beta/float(big-v.battery_life))
     w.ex = w.ex + (beta/float(big-w.battery_life))
     if v.ex == 1:
--- ctno.py
+++ ctno.py
@@ -9,13 +9,14 @@ import random as ran
 import scipy as sci
 import copy
 import cove as cov
+import geom as geo
 from obal import G as G
 import bild as bil
 import caut as cau
 
 class T_Node(sim.Process):
     Next_id = 0
-    def __init__(self):
+    def __init__(self, parent):
         sim.Process.__init__(self, name="t-node"+str(T_Node.Next_id))
         self.id = T_Node.Next_id
         self.battery_life = ran.randint(100,150)
@@ -31,7 +32,7 @@ class T_Node(sim.Process):
         self.ex = 0
         self.current_cover = None
         self.current_cover_index = 0
-
+        self.parent = parent
     def dup(self):
         n = T_Node()
         n.x = self.x
@@ -41,17 +42,28 @@ class T_Node(sim.Process):
 
     def run(self):
         while 1:
-            print self.id, self.battery_life, self.on, [a.uv for a in self.targets], [a.id for a in self.neighbors]
-            now = sim.now()
             cau.t_algorithm(self)
+            print "time: ", sim.now(), "t_node: ", self.id, self.battery_life, self.on, [a.uv for a in self.targets], [a.id for a in self.neighbors]
+            now = sim.now()
             if self.on:
+                k = geo.key_targets(self)
+                for a in self.neighbors:
+                    k[a.id].covered = True
+                print "t_node: ", self.id
                 yield sim.hold, self, self.battery_life
                 self.battery_life = 0
+                self.parent.output(sim.now())
                 self.on = False
+                self.ex = 0
+                for a in self.neighbors:
+                    if not a.on:
+                        k[a.id].covered = False
+                        k[a.id].active = False
+                        k[a.id].star_edge = False
                 for a in self.neighbors:
                     if a.battery_life > 0:
-                        a.on
                         sim.reactivate(a)
+                        cau.t_algorithm(a)
                 print ("t_node %s died at %d" % (self.id, sim.now()))
             else:
                 yield sim.passivate, self
