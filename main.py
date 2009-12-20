444a1eb748e3db9724c6c3e1c598d329e2a172d6
j.paul daigle <jdaigle1@student.gsu.edu>
2009-12-20 01:26:43 -0500

    added command line sugar

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
+import sys, getopt
 import random as ran
 import netw
 import nect
-import obal
+from obal import G as G
 
 def main():
-    for i in range(4):
-        t_source_out = open(("%d_sourc2.dat" %(i)), 'w')
-        source_out = open(("%d_source.dat" %(i)), 'w')
-        for j in range(20):
-            net = netw.NodeSource(source_out)
-            ne2 = nect.T_NodeSource(t_source_out)
-            net.generate(5+5*i, 5)
+    try:
+        opts, atgs = getopt.getopt(sys.argv[1:], "fi:r:b:a:", ["iterations=", "runs=", "base=", "area="])
+    except getopt.GetoptError:
+        sys.exit(2)
+    framework_only = False
+    i=4
+    b=5
+    r=20
+    for o,a in opts:
+        if o in ('-f'):
+            framework_only = True
+        if o in ('-i', '--iterations'):
+            i=int(a)
+        if o in ('-r','--runs'):
+            r =int(a)
+        if o in ('-b','--base'):
+            b=int(a)
+        if o in ('-a','--area'):
+            G.bounds = int(a)
+    cresult_out = open("c_data.dat", 'w')
+    fresult_out = open("f_data.dat", 'w')
+    for i in range(i):
+        fsize, flife, csize, clife = 0,0,0,0
+        count = 0
+        for j in range(r):
+            net = netw.NodeSource()
+            ne2 = nect.T_NodeSource()
+            net.generate(b+b*i, 5)
             net.feed_nect(ne2)
-            net.go()
-            ne2.go()
+            fsizt, flift = net.go()
+            if not framework_only:
+                csizt, clift = ne2.go()
+            else:
+                csizt, clift = 5, 5
+            if csizt != 0:
+                fsize += fsizt
+                flife += flift
+                csize += csizt
+                clife += clift
+                count += 1
+        fsiza = fsize / float(count)
+        flifa = flife / float(count)
+        csiza = csize / float(count)
+        clifa = clife / float(count)
+        fresult_out.write(("%d\t%d\t%d\n" %(b+b*i, fsiza, flifa)))
+        cresult_out.write(("%d\t%d\t%d\n" %(b+b*i, csiza, clifa)))
 main()
--- obal.py
+++ obal.py
@@ -1,7 +1,7 @@
 import random as ran
 
 class G(object):
-    bound = 1000
+    bound = 800
     sensor_range = 80
     comm_range = 160
     big_constant = 1000000
